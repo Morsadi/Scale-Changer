36e83d0b34054428d87a25340f74626505e42d0a
Morsadi <badreddine.morsadi@github.com>
2019-08-05 15:54:17 -0700

    Get Chords

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import musicalNotes from './musicalNote';
 import Chord from './Chord/chord';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+
+
 
 class App extends Component{
 
@@ -12,47 +16,72 @@ class App extends Component{
     this.state= {
 
       musicalNotes : musicalNotes,
-      chord1 : '',
+      chords : [],
+      chordCount:[],
       targetChord: '',
       }
 
 
   }
 
-eventHandler(e){
-  this.setState({
-    [e.target.name]: e.target.value
-  })
-}
+
 
 getNotes(){
 
+//loops through all the chords and stores the checked chords in the state
+let chords = document.getElementsByClassName('select-css');
 
 
-
-
-
-
-
-
+for (let i = 0; i < chords.length; i++){
+  if (chords){
+    let currentChord = chords[i].value;
+    let newList = this.state.chords;
+    newList.push(currentChord)
+    this.setState({
+      chords: newList
+    })
+  }
+  
 }
 
 
+}
+//add a chord dropdown
+addChord(){
+ let previousChords = this.state.chordCount;
+ let count = previousChords.length +1
+ previousChords.push([count, ''])
+ this.setState({
+   chordCount: previousChords
+ })
+
+}
 
 render(){
   return (
 
 
-
-
-
     <div className="App">
 
+
     
-   <Chord />
+    {
+      this.state.chordCount.map((e, index)=>
+      <select key={index} className="select-css">
+	<option>C</option>
+	<option>D</option>
+	<option>E</option>
+	<option>F</option>
+	<option>G</option>
+  <option>A</option>
+  <option>B</option>
+</select>
+      )
+    }
+   
+    <input onClick={this.addChord.bind(this)} type='button' value='+' />
 
-
-    <input type='button' value='Transform' onClick={this.getNotes.bind(this)}/>
+    <input onClick={this.getNotes.bind(this)}  type='button' value='Transform' onClick={this.getNotes.bind(this)}/>
 
    </div>
   )
--- src/Chord/chord.js
+++ src/Chord/chord.js
@@ -13,62 +13,61 @@ class Chord extends Component{
       musicalNotes : musicalNotes,
       chord : '',
       targetChord: '',
+      selectValue: ''
       }
 
 
   }
 
-  eventHandler(e){
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  getNotes(){
-    const allNotes = this.state.musicalNotes;
-   
-  const firstChord = this.state.chord1;
-  const target = this.state.targetChord;
-  let key1;
-  let key2;
-  let targetChord;
-  let count;
-  
-  
-  //first chord
-  for(let i in allNotes){
-  if (firstChord === allNotes[i].chord){
-    const key = allNotes[i].key
-    key1= key;
-  
-    break;
-  } 
-  }
  
-  //target chord
-  for(let i = 0; i <= allNotes.length; i++){
-    if(target === allNotes[i].chord){
-      const key = allNotes[i].key
-    targetChord= key;
-    
-    
-  
-      break;
-    }
+eventHandler(e){
+    this.setState({selectValue:e.target.value});
   }
+  getNotes(){
+//     const allNotes = this.state.musicalNotes;
+   
+//   const firstChord = this.state.chord1;
+//   const target = this.state.targetChord;
+//   let key1;
+//   let key2;
+//   let targetChord;
+//   let count;
   
   
-  //finding the count between the chords
-    count= targetChord - key1
-  let newChord = key2 + count; 
-  //find chords
-    for(let i in allNotes){
-      if(newChord === allNotes[i].key){
-        const chord = allNotes[i].chord
-        newChord= chord;
-        break;
-      }
-    }
+//   //first chord
+//   for(let i in allNotes){
+//   if (firstChord === allNotes[i].chord){
+//     const key = allNotes[i].key
+//     key1= key;
+  
+//     break;
+//   } 
+//   }
+ 
+//   //target chord
+//   for(let i = 0; i <= allNotes.length; i++){
+//     if(target === allNotes[i].chord){
+//       const key = allNotes[i].key
+//     targetChord= key;
+    
+    
+  
+//       break;
+//     }
+//   }
+  
+  
+//   //finding the count between the chords
+//     count= targetChord - key1
+//   let newChord = key2 + count; 
+//   //find chords
+//     for(let i in allNotes){
+//       if(newChord === allNotes[i].key){
+//         const chord = allNotes[i].chord
+//         newChord= chord;
+//         break;
+//       }
+//     }
   
   }
 
@@ -81,16 +80,21 @@ class Chord extends Component{
   <div>
 
 
-<input type='text' name='chord' placeholder='Your First Chord' name='chord' onChange={this.eventHandler.bind(this)} />
+<select value={this.state.selectValue} onChange={this.eventHandler.bind(this)} className="select-css">
+	<option className="chords" id="1">C</option>
+	<option className="chords" id="2">D</option>
+	<option className="chords" id="3">E</option>
+	<option className="chords" id="4">F</option>
+	<option className="chords" id="5">G</option>
+  <option className="chords" id="6">A</option>
+  <option className="chords" id="7">B</option>
+</select>
 
 
   </div>
         )
     }
-    
-    
-    
-    }
+}
     
     
     
