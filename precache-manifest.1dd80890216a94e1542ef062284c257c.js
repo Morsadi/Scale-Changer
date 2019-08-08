44c1632002570f6d1352699c4883f800e0b486bd
Morsadi <badreddine.morsadi@github.com>
2019-08-08 13:45:50 -0700

    Updates

---
+++ precache-manifest.1dd80890216a94e1542ef062284c257c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b017da0fba0dcb356a7640c4b4184b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b017d3621a284358e997",
+    "url": "./static/css/main.92af33db.chunk.css"
+  },
+  {
+    "revision": "f56192beaa7ff71843f9",
+    "url": "./static/js/2.251d2fa7.chunk.js"
+  },
+  {
+    "revision": "b017d3621a284358e997",
+    "url": "./static/js/main.53168f57.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Scale-Changer/precache-manifest.2672749ca2481bbd0927a4639351ccf0.js"
+  "./precache-manifest.1dd80890216a94e1542ef062284c257c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Scale-Changer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
