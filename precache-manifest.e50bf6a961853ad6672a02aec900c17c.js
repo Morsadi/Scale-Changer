0a5df234de542e61c88350c453fd9c9af04e382b
Morsadi <badreddine.morsadi@github.com>
2019-08-08 12:01:46 -0700

    Updates

---
+++ precache-manifest.e50bf6a961853ad6672a02aec900c17c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a76eed8962d17ab4696a6ef99705da8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ec9692e30131d22b99",
+    "url": "/static/css/main.92af33db.chunk.css"
+  },
+  {
+    "revision": "f56192beaa7ff71843f9",
+    "url": "/static/js/2.251d2fa7.chunk.js"
+  },
+  {
+    "revision": "88ec9692e30131d22b99",
+    "url": "/static/js/main.2fb50d4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
